cd70484d4b5ad3f47506278c9b08f9927bc85f9d
503945930 <503945930@qq.com>
2018-02-26 22:21:39 +0800

    script

--- startup_test.sh
+++ startup_test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3.6 main.py -m mongodb://127.0.0.1:27017 -d testnet-node -r http://future.otcgo.cn:20332 
+python3 main.py -m mongodb://127.0.0.1:27017 -d testnet-node -r http://future.otcgo.cn:20332 
