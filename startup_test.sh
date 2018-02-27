4c289a96b53d8603cf4b16d8f76a7ef1fd02818e
503945930 <503945930@qq.com>
2018-02-27 15:06:04 +0800

    fix

--- startup_test.sh
+++ startup_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# python3 main.py -m mongodb://127.0.0.1:27017 -d testnet-node -r http://future.otcgo.cn:20332 
+python3 main.py -m mongodb://127.0.0.1:27017 -d testnet-node -r http://future.otcgo.cn:20332 
 
-python3 main.py -m mongodb://future.otcgo.cn:27017 -d testnet-node -r http://future.otcgo.cn:20332 
+# python3 main.py -m mongodb://future.otcgo.cn:27017 -d testnet-node -r http://future.otcgo.cn:20332 
