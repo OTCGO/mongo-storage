0a2c7a233889c393274ed5405a86712c01bdb3ae
503945930 <503945930@qq.com>
2019-04-15 22:42:15 +0800

    fix

--- asset_rank.py
+++ asset_rank.py
@@ -47,10 +47,12 @@ def main(start,end):
             for vout in result["vout"]:
                 handle_utxo(vout["address"],vout["asset"],result["blockIndex"])
 
-
-            if result["nep5"] is None:
+            if result.has_key('nep5') is False:
                 continue
                 
+            if result["nep5"] is None:
+                continue
+
             for nep5 in result["nep5"]:
                 handle_nep5(nep5["to"],nep5["assetId"],result["blockIndex"])
                 handle_nep5(nep5["from"],nep5["assetId"],result["blockIndex"])
